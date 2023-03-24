498fb4a366384426317d9ab21431e86dce65fb89
FabKaiz <cacrfabfr@gmail.com>
2023-03-24 11:57:49 +0100

    [!] Fix: modal close btn and code format

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,28 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
+const contentModal = document.querySelector(".content");
+
+console.log(contentModal);
+
+const closeModal = () => {
+  // Add animation class to modal
+  contentModal.classList.add("close-animation");
+  // Get animation duration from css variable
+  const animationDuration = getComputedStyle(document.documentElement).getPropertyValue('--modal-duration');
+  // convert duration to ms and remove the "s" at the end
+  const convertedAnimationDuration = parseFloat(animationDuration) * 1000;
+
+  // Close modal after animation duration
+  setTimeout(() => {
+    contentModal.classList.remove("close-animation");
+    modalbg.style.display = "none";
+  }, convertedAnimationDuration);
+}
+
+// Close modal event
+closeBtn.addEventListener("click", closeModal);
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
